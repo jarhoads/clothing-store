0c913e6bfba92a11997cd0b43b2c3e88b631c84e
Josh <jarhoads@gmail.com>
2024-07-23 10:17:17 -0400

    adding context

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import { useState } from 'react';
 import Header from './components/Header.jsx';
 import Shop from './components/Shop.jsx';
 import Product from './components/Product.jsx';
+
 import { DUMMY_PRODUCTS } from './dummy-products.js';
+
+import { CartContext } from './store/shopping-cart-context.jsx';
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -66,8 +69,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart
+  };
+
   return (
-    <>
+    <CartContext.Provider value={ctxValue}>
       <Header cart={shoppingCart} onUpdateCartItemQuantity={handleUpdateCartItemQuantity} />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
@@ -76,7 +84,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </>
+    </CartContext.Provider>
   );
 }
 
